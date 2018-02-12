bf13efd38519d82773f778ea4860c181ab63dd75
Stuart Tannehill <stuart.tannehill@robotsandpencils.com>
2018-02-12 16:16:13 -0500

    added loading silliness

--- src/components/launches.vue
+++ src/components/launches.vue
@@ -7,40 +7,44 @@
       <label class="custom-checkbox">
         <input type="checkbox" value="launch_success">Land Success
         <span class="checkmark"></span>
-        </label>
-        <label class="custom-checkbox">
-          <input type="checkbox" value="reused">Reused
-          <span class="checkmark"></span>
-          </label>
-          <label class="custom-checkbox">
-            <input type="checkbox" value="reddit">With Reddit
-            <span class="checkmark"></span>
-            </label>
-            </form>
-            <p class="showing">{{this.results.length}} launches</p>
-            </div>
-    <table class="launch-table">
-        <thead>
-          <th>Badge</th>
-          <th>Rocket Name</th>
-          <th>Rocket Type</th>
-          <th class="launch-date" :class="sortorder" @click="sorter">Launch Date</th>
-          <th>Details</th>
-          <th>ID</th>
-          <th>Article</th>
-        </thead>
-        <tbody v-for="(result, key) in results" :key="key">
-          <tr>
-            <td class="badge"><img :src="result.links.mission_patch"></td>
-            <td>{{result.rocket.rocket_name}}</td>
-            <td>{{result.rocket.rocket_type}}</td>
-            <td class="launch-date">{{result.launch_date_local | formatDate}}</td>
-            <td class="details">{{result.details | trimString}}<span v-if="result.details" class="tooltip">{{result.details}}</span></td>
-            <td>{{result.flight_number}}</td>
-            <td class="article"><a v-if="result.links.article_link" class="link" :href="result.links.article_link"></a></td>
-          </tr>
-        </tbody>
-    </table>
+      </label>
+      <label class="custom-checkbox">
+        <input type="checkbox" value="reused">Reused
+        <span class="checkmark"></span>
+      </label>
+      <label class="custom-checkbox">
+        <input type="checkbox" value="reddit">With Reddit
+        <span class="checkmark"></span>
+      </label>
+    </form>
+    <p class="showing">{{this.results.length}} launches</p>
+  </div>
+  <div class="loading" v-if="isLoading">
+    <img src="../assets/images/loading-rocket.png" >
+    <h2>...Loading</h2>
+    </div>
+  <table v-else class="launch-table">
+      <thead>
+        <th>Badge</th>
+        <th>Rocket Name</th>
+        <th>Rocket Type</th>
+        <th class="launch-date" :class="sortorder" @click="sorter">Launch Date</th>
+        <th>Details</th>
+        <th>ID</th>
+        <th>Article</th>
+      </thead>
+      <tbody v-for="(result, key) in results" :key="key">
+        <tr>
+          <td class="badge"><img :src="result.links.mission_patch"></td>
+          <td>{{result.rocket.rocket_name}}</td>
+          <td>{{result.rocket.rocket_type}}</td>
+          <td class="launch-date">{{result.launch_date_local | formatDate}}</td>
+          <td class="details">{{result.details | trimString}}<span v-if="result.details" class="tooltip">{{result.details}}</span></td>
+          <td>{{result.flight_number}}</td>
+          <td class="article"><a v-if="result.links.article_link" class="link" :href="result.links.article_link"></a></td>
+        </tr>
+      </tbody>
+  </table>
   <div class="rocket"></div>
 </div>
 </template>
@@ -55,7 +59,8 @@ export default {
     return {
       results: [],
       error: [],
-      sortorder: 'asc'
+      sortorder: 'asc',
+      isLoading: true
     }
   },
   methods: {
@@ -72,12 +77,15 @@ export default {
     }
   },
   created () {
+    this.isLoading = true
     axios.get('https://api.spacexdata.com/v2/launches')
       .then(res => {
         this.results = res.data
+        this.isLoading = false
         console.log('data says: ', this.results)
       })
       .catch(e => {
+        this.isLoading = false
         this.errors.push(e)
       })
   }
